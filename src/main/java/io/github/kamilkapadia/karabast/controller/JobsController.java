49c05e20a6ac80975d75c3bd79b389c93615b4d4
Kamil Kapadia <kamilkapadia@gmail.com>
2021-03-02 23:36:54 -0800

    Updates for job details page and link from jobs list to job detail page for each job

--- src/main/java/io/github/kamilkapadia/karabast/controller/JobsController.java
+++ src/main/java/io/github/kamilkapadia/karabast/controller/JobsController.java
@@ -1,14 +1,25 @@
 package io.github.kamilkapadia.karabast.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import io.github.kamilkapadia.karabast.dto.ActionDTO;
+import io.github.kamilkapadia.karabast.dto.lookup.ActionCode;
+import io.github.kamilkapadia.karabast.dto.lookup.StatusCode;
+import io.github.kamilkapadia.karabast.dto.setup.Action;
 import io.github.kamilkapadia.karabast.dto.setup.Job;
 import io.github.kamilkapadia.karabast.service.setup.ContentPathService;
+import io.github.kamilkapadia.karabast.service.lookup.ActionCodeService;
+import io.github.kamilkapadia.karabast.service.lookup.StatusCodeService;
 import io.github.kamilkapadia.karabast.service.setup.ActionService;
 import io.github.kamilkapadia.karabast.service.setup.HistoricalNameService;
 import io.github.kamilkapadia.karabast.service.setup.JobService;
@@ -32,6 +43,11 @@ public class JobsController {
 	@Autowired
 	private ActionService actionService;
 	
+	@Autowired
+	private StatusCodeService statusCodeService;
+	
+	@Autowired
+	private ActionCodeService actionCodeService;
 	
 	@GetMapping("/jobs")
 	public String jobs(Model theModel) {
@@ -42,11 +58,60 @@ public class JobsController {
 	}
 	
 	@GetMapping("/job-details")
-	public String jobDetails(Model theModel) {
-		theModel.addAttribute("rules", ruleService.findByJobId(1));
-		theModel.addAttribute("historical", historicalNameService.findByJobId(1));
-		theModel.addAttribute("content", contentPathService.findByJobId(1));
-		theModel.addAttribute("actions", actionService.findByJobId(1));
+	public String jobDetails(@RequestParam("jobId") int theId, Model theModel) {
+		theModel.addAttribute("jobs", jobService.findById(theId));
+		theModel.addAttribute("rules", ruleService.findByJobId(theId));
+		theModel.addAttribute("historical", historicalNameService.findByJobId(theId));
+		theModel.addAttribute("content", contentPathService.findByJobId(theId));
+		
+		Map<Integer, String> statusCodeMap = new HashMap<Integer, String>();
+		List<StatusCode> statusCodes = statusCodeService.findAll();
+		
+		for (StatusCode statusCode : statusCodes) {
+			statusCodeMap.put(statusCode.getId(), statusCode.getName());
+		}
+		
+		Map<Integer, String> actionCodeMap = new HashMap<Integer, String>();
+		List<ActionCode> actionCodes = actionCodeService.findAll();
+		
+		for (ActionCode actionCode : actionCodes) {
+			actionCodeMap.put(actionCode.getId(), actionCode.getName());
+		}
+		
+		
+		
+		List<ActionDTO> actionDTOs = new ArrayList<ActionDTO>();
+		
+		List<Action> actions = actionService.findByJobId(theId);
+		
+		for (Action action : actions) {
+			ActionDTO actionDTO = new ActionDTO();
+			
+			actionDTO.setActive(action.isActive());
+			
+			int statusMask = action.getTypeMask();
+			
+			for (StatusCode statusCode : statusCodes) {
+				if ( (statusMask & statusCode.getId()) > 0) {
+					actionDTO.getStatuses().add(statusCodeMap.get(statusCode.getId()));
+				}
+			}
+			
+			int actionMask = action.getActionMask();
+			
+			for (ActionCode actionCode : actionCodes) {
+				if ( (actionMask & actionCode.getId()) > 0) {
+					actionDTO.getActions().add(actionCodeMap.get(actionCode.getId()));
+				}
+			}
+			
+			
+			actionDTOs.add(actionDTO);
+		}
+		
+		theModel.addAttribute("actions", actionDTOs);
+		
+		
 		
 		return "job-details";
 	}
---
+++ src/main/java/io/github/kamilkapadia/karabast/dto/ActionDTO.java
@@ -0,0 +1,29 @@
+package io.github.kamilkapadia.karabast.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActionDTO {
+	private List<String> statuses = new ArrayList<String>();
+	private List<String> actions = new ArrayList<String>();
+	private boolean active;
+	
+	public List<String> getStatuses() {
+		return statuses;
+	}
+	public void setStatuses(List<String> statuses) {
+		this.statuses = statuses;
+	}
+	public List<String> getActions() {
+		return actions;
+	}
+	public void setActions(List<String> actions) {
+		this.actions = actions;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+}
